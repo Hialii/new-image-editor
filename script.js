5b15127f293ce917fa2fe38ac17577a8ef944c25
Hiali <hialidias@gmail.com>
2023-03-03 10:41:10 -0300

    final

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ filterName = document.querySelector(".filter-info .name"),
 filterValue = document.querySelector(".filter-info .value"),
 filterSlider = document.querySelector(".slider input"),
 previewImg = document.querySelector(".preview-img img"),
-chooseImgBtn = document.querySelector(".choose-image");
+chooseImgBtn = document.querySelector(".choose-image"),
+resetFilterBtn = document.querySelector(".reset-filter"),
+saveImgBtn = document.querySelector(".save-image");
 
 let brightness= 100, saturation = 100, inversion = 0, grayscale = 0;
 
@@ -14,7 +16,9 @@ const loadImage = () => {
    if(!file) return;
    previewImg.src = URL.createObjectURL(file);
    previewImg.addEventListener('load', () => {
+      resetFilterBtn.click();
       document.querySelector(".container").classList.remove("disabled");
+      
    });
    
 };
@@ -46,6 +50,10 @@ filterOptions.forEach(option => {
    });
 });
 
+const applyFilters = () => {
+   previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
+}
+
 const updateFilter = () => {
    filterValue.textContent = `${filterSlider.value}%`
    const selectedFilter = document.querySelector(".filter .active");  //pega o filtro selecionado
@@ -60,10 +68,31 @@ const updateFilter = () => {
    }else {
       grayscale = filterSlider.value;
    }
+   applyFilters();
+};
+
+const resetFilter = () => {
+   brightness= 100, saturation = 100, inversion = 0, grayscale = 0; //reseta todas a variavéis para o valor inicial
+   applyFilters();
 }
 
+const saveImage = () => {
+   const canvas = document.createElement("canvas"); //cria um elemento canvas
+   const ctx = canvas.getContext("2d"); //canvas.getContext return a drawing contex on the canvas
+   canvas.width = previewImg.naturalWidth; //salva a img com largura original
+   canvas.height = previewImg.naturalHeight; //salva a imagem com a altura original
+   
+   ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+   ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);//drawImage(imagem, dx, dy, dWidth, dHeight)
+   
+   const link = document.createElement("a");
+   link.download = "image.jpg"; 
+   link.href = canvas.toDataURL();
+   link.click();
+ }
 
-
-filterSlider.addEventListener("input", updateFilter)
+filterSlider.addEventListener("input", updateFilter);
 fileInput.addEventListener("change", loadImage);
+resetFilterBtn.addEventListener("click", resetFilter);
+saveImgBtn.addEventListener("click", saveImage)
 chooseImgBtn.addEventListener("click", () => fileInput.click());
